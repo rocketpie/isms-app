d7eb6db53eaf12d9d33030f32850d69f5c777db8
rocketpie <mail.l.conrad@gmail.com>
2025-09-15 21:59:41 +0200

    start testing the API functions

--- docker/restart.sh
+++ docker/restart.sh
@@ -20,3 +20,7 @@ docker exec -i isms-app-db-1 sh -lc '
   export PGPASSWORD="$POSTGRES_PASSWORD";
   psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h 127.0.0.1
 ' < supabase/migrations/020_policies.sql
+
+
+# restart postgrest to reload schema cache
+docker compose restart postgrest
